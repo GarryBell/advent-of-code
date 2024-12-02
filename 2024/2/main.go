a07d8991480b098ad06d127b4f377e433e6660d1
Roman Bell <r.g.bell@researchable.nl>
2024-12-02 19:02:34 +0100

    feat: very simple and inefficient solution

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -4,50 +4,63 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-func checkRecord(record string) bool {
-	var recordArr = strings.Split(record, " ")
+func RemoveIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
+}
+
+func checkRecord(recordArr []string) bool {
 	first, _ := strconv.Atoi(recordArr[0])
 	second, _ := strconv.Atoi(recordArr[1])
-	canRemove := true
 	isAscending := first < second
-	for i := 0; i < (len(recordArr) - 1); i++ {
+	for i := 0; i < (len(recordArr) - 2); i++ {
 		current, _ := strconv.Atoi(recordArr[i])
 		next, _ := strconv.Atoi(recordArr[i+1])
 		diff := next - current
 		if !isAscending {
 			diff = -diff
 		}
-		if 1 > diff || diff > 3 {			
-			if(canRemove){
-				canRemove = false
-				recordArr = append(recordArr[:i], recordArr[i+1:]...)
-				i = i - 1
-			}else{
-				return false
-
-			}
+		if 1 > diff || diff > 3 {
+			return false
+		}
 
 	}
 	return true
 }
 
 func main() {
-	filePath := "input_small.txt"
+	filePath := "input.txt"
 	readFile, _ := os.Open(filePath)
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
 	count := 0
+	countNormal := 0
 	for fileScanner.Scan() {
+		var recordArr = strings.Split(fileScanner.Text(), " ")
+		var current = false
 
-		if checkRecord(fileScanner.Text()) {
+		if checkRecord(recordArr) {
+			current = true
+			countNormal += 1
+		}
+
+		for i := 0; i < len(recordArr); i++ {
+			var arr = slices.Clone(recordArr)
+			RemoveIndex(arr, i)
+			if checkRecord(arr) {
+				current = true
+			}
+		}
+		if current {
 			count += 1
 		}
 	}
+	fmt.Println(countNormal)
 	fmt.Println(count)
 
 }
